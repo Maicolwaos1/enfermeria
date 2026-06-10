9f27777dae8ccf87ada153b174e54c8085f3adbb
Fabián de Jesús de Lira Rodríguez <up240231@alumnos.upa.edu.mx>
2026-06-09 19:36:52 -0600

    Estructura inicial: Backend, Frontend y SQL

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="contenedor">
+      <h1 className="titulo">¡Frontend con React Listo!</h1>
+      <p>Ahora estás usando React puro con CSS tradicional, sin Tailwind.</p>
+      <button className="boton-estilo">Botón de Prueba</button>
+    </div>
+  )
+}
+
+export default App
